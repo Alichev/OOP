226f77a21b8f935e1a816a9d2f2a04f1b7849fba
Anton Alichev <tosiakz@yandex.ru>
2023-03-09 14:18:49 +0300

    add Lecrion 1, Seminar 1

---
+++ Lections/Ex_002/Point2D.java
@@ -0,0 +1,5 @@
+package Lections.Ex_002;
+
+public class Point2D {
+    int x, y;
+}
---
+++ Seminars/Units/MagClass.java
@@ -0,0 +1,10 @@
+package Seminars.Units;
+
+public class MagClass extends BaseHero {
+
+    public MagClass(String name, int hp) {
+        super(name, hp);
+
+    }
+
+}
